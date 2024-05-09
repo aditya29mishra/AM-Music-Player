a4e475ec3bd4a34540579d4923c34ea4fab4f22e
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-10 02:05:26 +0530

    refactor MainActivity

--- app/src/main/java/com/scarry/ammusicplayer/MainActivity.kt
+++ app/src/main/java/com/scarry/ammusicplayer/MainActivity.kt
@@ -9,9 +9,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.insets.ProvideWindowInsets
 import com.scarry.ammusicplayer.ui.theme.AMMusicPlayerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,13 +22,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AMMusicPlayerTheme {
-
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background,
-                    content = { })
-
+                ProvideWindowInsets{
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.background
+                    ) {
+                        Greeting("Android")
+                    }
+                }
             }
         }
     }
 }
 
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AMMusicPlayerTheme {
+        Greeting("Android")
+    }
+}
