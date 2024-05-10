86bf0f7aeb9d9a41be37a8b7a06994e6d8e8dcf3
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-10 23:33:57 +0530

    refactor TokenManager.kt

--- app/src/main/java/com/scarry/ammusicplayer/data/remote/token/tokenManager/TokenManager.kt
+++ app/src/main/java/com/scarry/ammusicplayer/data/remote/token/tokenManager/TokenManager.kt
@@ -1,5 +1,6 @@
 package com.scarry.ammusicplayer.data.remote.token.tokenManager
 
+import com.google.gson.internal.GsonBuildConfig
 import com.scarry.ammusicplayer.data.remote.musicservice.SpotifyEndPoints
 import com.scarry.ammusicplayer.data.remote.token.AccessTokenResponseDTO
 import retrofit2.Response
@@ -7,12 +8,18 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Header
 import retrofit2.http.POST
+import java.util.Base64
 
-interface SpotifyTokenManager : TokenManager<Response<AccessTokenResponseDTO>> {
+//val SPOTIFY_CLIENT_SECRET_BASE64 : String
+//    get() = Base64.encodeToString(
+//        "Basic ${BuildConfig.SPOTIFY_CLIENT_ID}:${BuildConfig.SPOTIFY_CLIENT_SECRET}".toByteArray(),
+//        Base64.NO_WRAP
+//    )
+interface TokenManager{
 
     @FormUrlEncoded
     @POST(SpotifyEndPoints.ApiTokenEndPoint)
-    override suspend fun getAccessToken(
+     suspend fun getAccessToken(
         @Field("grant_type") grantType: String,
         @Header("Authorization") secret: String
     ): Response<AccessTokenResponseDTO>
