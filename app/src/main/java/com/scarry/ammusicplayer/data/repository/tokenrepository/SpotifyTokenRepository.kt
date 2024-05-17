9fefba447a0cba527ec06f5412209cfa3b499f47
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-18 00:43:38 +0530

    refactor SpotifyTokenRepository for Token Expiration

--- app/src/main/java/com/scarry/ammusicplayer/data/repository/tokenrepository/SpotifyTokenRepository.kt
+++ app/src/main/java/com/scarry/ammusicplayer/data/repository/tokenrepository/SpotifyTokenRepository.kt
@@ -3,6 +3,7 @@ package com.scarry.ammusicplayer.data.repository.tokenrepository
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.scarry.ammusicplayer.data.remote.token.BearerToken
+import com.scarry.ammusicplayer.data.remote.token.isExpired
 import com.scarry.ammusicplayer.data.remote.token.toBearerToken
 import com.scarry.ammusicplayer.data.remote.token.tokenManager.SPOTIFY_CLIENT_SECRET_BASE64
 import com.scarry.ammusicplayer.data.remote.token.tokenManager.TokenManager
@@ -15,12 +16,8 @@ class SpotifyTokenRepository(
     private var token : BearerToken? = null
     @RequiresApi(Build.VERSION_CODES.O)
     override suspend fun getBearerToken(): BearerToken {
-        if (token == null) getAndAssignToken()
-        return token!!.apply {
-            val timeOfExpiration = timeOfCreation.plusSeconds(secondsUntilExpiration.toLong())
-            val tokenExpired = LocalDateTime.now() > timeOfExpiration
-            if (tokenExpired) getAndAssignToken()
-        }
+        if (token == null || token?.isExpired == true) getAndAssignToken()
+        return token!!
 
     }
 
