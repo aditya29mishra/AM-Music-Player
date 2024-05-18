3116f3140028c0f984362112803487b2dc0a262a
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-18 19:28:54 +0530

    injected AndroidBase64Encoder

--- app/src/main/java/com/scarry/ammusicplayer/data/encoder/AndroidBase64Encoder.kt
+++ app/src/main/java/com/scarry/ammusicplayer/data/encoder/AndroidBase64Encoder.kt
@@ -1,8 +1,9 @@
 package com.scarry.ammusicplayer.data.encoder
 
 import android.util.Base64
+import javax.inject.Inject
 
-class AndroidBase64Encoder : Base64Encoder {
+class AndroidBase64Encoder @Inject constructor() : Base64Encoder {
     override fun encodeToString(
         input: ByteArray
     ): String = Base64.encodeToString(input, android.util.Base64.NO_WRAP)
