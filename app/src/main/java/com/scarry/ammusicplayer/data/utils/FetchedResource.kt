11f6cd4aac454eb8709e9406bade7c23e5a56c68
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-18 15:59:34 +0530

    added FetchedResource.kt and MapperImageSize

---
+++ app/src/main/java/com/scarry/ammusicplayer/data/utils/FetchedResource.kt
@@ -0,0 +1,20 @@
+package com.scarry.ammusicplayer.data.utils
+
+import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities.Private
+
+/**
+ * A sealed class hierarchy that is used to encapsulate a fetched
+ * resource of type [ResourceType]. If any error occurs, the
+ * [FailureType] will be used.
+ */
+sealed class FetchedResource<ResourceType , FailureType> {
+    data class Success<ResourceType , FailureType> (
+        private val data: ResourceType
+    ): FetchedResource<ResourceType , FailureType>()
+
+    data class Failure<ResourceType , FailureType> (
+        private val error: FailureType,
+        private val data: ResourceType? = null
+    ): FetchedResource<ResourceType , FailureType>()
+
+}
---
+++ app/src/main/java/com/scarry/ammusicplayer/data/utils/MapperImageSize.kt
@@ -0,0 +1,7 @@
+package com.scarry.ammusicplayer.data.utils
+
+enum class MapperImageSize {
+    SMALL ,
+    MEDIUM ,
+    LARGE
+}
