1a8108cf9442edf5181db56c0c75483b898b6fc4
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-18 15:35:45 +0530

    refactor TokenManager

--- app/src/main/java/com/scarry/ammusicplayer/data/remote/token/tokenManager/TokenManager.kt
+++ app/src/main/java/com/scarry/ammusicplayer/data/remote/token/tokenManager/TokenManager.kt
@@ -9,15 +9,7 @@ import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Header
 import retrofit2.http.POST
 
-@Deprecated(
-    message = " use SpotifyClientSecret(base64encoder: Base64Encoder) instead",
-    replaceWith = ReplaceWith("getSpotifyClientSecret()")
-)
-val SPOTIFY_CLIENT_SECRET_BASE64 : String
-    get() = Base64.encodeToString(
-        "Basic ${BuildConfig.SPOTIFY_CLIENT_ID}:${BuildConfig.SPOTIFY_CLIENT_SECRET}".toByteArray(),
-        Base64.NO_WRAP
-    )
+
 
 const val defaultGrantType = "client_credentials"
 
