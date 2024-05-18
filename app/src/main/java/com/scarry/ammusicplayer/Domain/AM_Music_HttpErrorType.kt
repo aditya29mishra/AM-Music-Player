e10d7ba73c3fdc21aed421c4045f64ada860cd82
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-18 16:13:32 +0530

    refactor ErrorType

--- app/src/main/java/com/scarry/ammusicplayer/Domain/AM_Music_HttpErrorType.kt
+++ app/src/main/java/com/scarry/ammusicplayer/Domain/AM_Music_HttpErrorType.kt
@@ -7,12 +7,14 @@ enum class AM_Music_HttpErrorType {
     BAD_OR_EXPIRED_TOKEN,
     BAD_AUTH_REQUEST,
     RATE_LIMIT_EXCEEDED,
+    INVALID_REQUEST,
     UNKNOWN_ERROR
 }
 
 val HttpException.musifyHttpErrorType: AM_Music_HttpErrorType
     get() =
         when (this.code()) {
+            400 -> AM_Music_HttpErrorType.INVALID_REQUEST
             401 -> AM_Music_HttpErrorType.BAD_OR_EXPIRED_TOKEN
             403 -> AM_Music_HttpErrorType.BAD_AUTH_REQUEST
             429 -> AM_Music_HttpErrorType.RATE_LIMIT_EXCEEDED
