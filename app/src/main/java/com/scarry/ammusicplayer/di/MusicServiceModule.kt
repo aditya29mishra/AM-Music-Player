e1c1f3796e17099ce34ff6388f6900f81e991475
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-18 19:57:57 +0530

    Added ServiceModules

---
+++ app/src/main/java/com/scarry/ammusicplayer/di/MusicServiceModule.kt
@@ -0,0 +1,30 @@
+package com.scarry.ammusicplayer.di
+
+import com.scarry.ammusicplayer.data.remote.musicservice.SpotifyService
+import com.scarry.ammusicplayer.data.remote.token.tokenManager.TokenManager
+import com.scarry.ammusicplayer.utils.defaultAM_MusicJacksonConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn (Singleton::class)
+object MusicServiceModule {
+    @Provides
+    @Singleton
+    fun PorivdeSpotifyModule(): SpotifyService = Retrofit.Builder()
+        .baseUrl("https://api.spotify.com/")
+        .addConverterFactory(defaultAM_MusicJacksonConverterFactory)
+        .build()
+        .create(SpotifyService::class.java)
+
+    @Provides
+    @Singleton
+    fun ProvidesTokenManeger() : TokenManager = Retrofit.Builder()
+        .baseUrl("https://accounts.spotify.com/")
+        .addConverterFactory(defaultAM_MusicJacksonConverterFactory)
+        .build()
+        .create(TokenManager::class.java)
+}
