78ecf47d2c4cc50bcc37041a8c99d26284e0ef0b
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-21 15:15:59 +0530

    use of SpotifyBaseUrl in MusicServiceModule

--- app/src/main/java/com/scarry/ammusicplayer/di/MusicServiceModule.kt
+++ app/src/main/java/com/scarry/ammusicplayer/di/MusicServiceModule.kt
@@ -1,5 +1,6 @@
 package com.scarry.ammusicplayer.di
 
+import com.scarry.ammusicplayer.data.remote.musicservice.SpotifyBaseUrls
 import com.scarry.ammusicplayer.data.remote.musicservice.SpotifyService
 import com.scarry.ammusicplayer.data.remote.token.tokenManager.TokenManager
 import com.scarry.ammusicplayer.utils.defaultAM_MusicJacksonConverterFactory
@@ -16,7 +17,7 @@ object MusicServiceModule {
     @Provides
     @Singleton
     fun PorivdeSpotifyModule(): SpotifyService = Retrofit.Builder()
-        .baseUrl("https://api.spotify.com/")
+        .baseUrl(SpotifyBaseUrls.API_URL)
         .addConverterFactory(defaultAM_MusicJacksonConverterFactory)
         .build()
         .create(SpotifyService::class.java)
@@ -24,7 +25,7 @@ object MusicServiceModule {
     @Provides
     @Singleton
     fun ProvidesTokenManeger() : TokenManager = Retrofit.Builder()
-        .baseUrl("https://accounts.spotify.com/")
+        .baseUrl(SpotifyBaseUrls.AUTHENTICATION_URL)
         .addConverterFactory(defaultAM_MusicJacksonConverterFactory)
         .build()
         .create(TokenManager::class.java)
