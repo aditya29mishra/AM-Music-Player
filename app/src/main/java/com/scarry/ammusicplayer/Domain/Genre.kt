b02a0aa6ddbd6faee3bded71cc1cf0c4bcb7b3e3
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-22 16:31:34 +0530

    added GenreType enum class in genre data class

--- app/src/main/java/com/scarry/ammusicplayer/Domain/Genre.kt
+++ app/src/main/java/com/scarry/ammusicplayer/Domain/Genre.kt
@@ -3,4 +3,17 @@ package com.scarry.ammusicplayer.Domain
 data class Genre(
     val id: String,
     val name: String,
-)
+){
+    enum class GenreType {
+        AMBIENT,
+        CHILL,
+        CLASSICAL,
+        DANCE,
+        ELECTRONIC,
+        METAL,
+        RAINY_DAY,
+        ROCK,
+        PIANO,
+        POP
+    }
+}
