0bc570137c4bd4199619948a364cb2329aaceffd
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-23 15:30:05 +0530

    injected constructor in AmMusicBackgroundMusicPlayer

--- app/src/main/java/com/scarry/ammusicplayer/musicPlayer/AmMusicBackgroundMusicPlayer.kt
+++ app/src/main/java/com/scarry/ammusicplayer/musicPlayer/AmMusicBackgroundMusicPlayer.kt
@@ -5,16 +5,21 @@ import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.ui.PlayerNotificationManager
 import com.google.android.exoplayer2.util.NotificationUtil
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
-class AmMusicBackgroundMusicPlayer (
-    context: Context,
+class AmMusicBackgroundMusicPlayer @Inject constructor(
+    @ApplicationContext context: Context,
     private val exoPlayer: ExoPlayer
-): MusicPlayer{
-    private val notificationManager = PlayerNotificationManager.Builder(context,NOTIFICATION_ID , NOTIFICATION_CHANNEL_ID).setChannelImportance(NotificationUtil.IMPORTANCE_LOW).build().apply {
-        setPlayer(exoPlayer)
-    }
+) : MusicPlayer {
+    private val notificationManager =
+        PlayerNotificationManager.Builder(context, NOTIFICATION_ID, NOTIFICATION_CHANNEL_ID)
+            .setChannelImportance(NotificationUtil.IMPORTANCE_LOW).build().apply {
+            setPlayer(exoPlayer)
+        }
+
     override fun playTrackFormUrlString(urlString: String) {
-        with(exoPlayer){
+        with(exoPlayer) {
             if (isPlaying) exoPlayer.stop()
 
             setMediaItem(MediaItem.fromUri(urlString))
@@ -30,11 +35,14 @@ class AmMusicBackgroundMusicPlayer (
     override fun stopPlayingTrack() {
         exoPlayer.stop()
     }
-    companion object{
-        private const val NOTIFICATION_CHANNEL_ID = "com.scarry.ammusicplayer.musicPlayer.MusicPlayerService.NOTIFICATION_CHANNEL_ID"
+
+    companion object {
+        private const val NOTIFICATION_CHANNEL_ID =
+            "com.scarry.ammusicplayer.musicPlayer.MusicPlayerService.NOTIFICATION_CHANNEL_ID"
         private const val NOTIFICATION_CHANNEL_NAME = "playback controls"
         private const val NOTIFICATION_ID = 1
-        private const val NOTIFICATION_CHANNEL_DISCRIPTION = "Notifications that are used to control playback"
+        private const val NOTIFICATION_CHANNEL_DISCRIPTION =
+            "Notifications that are used to control playback"
 
     }
 
