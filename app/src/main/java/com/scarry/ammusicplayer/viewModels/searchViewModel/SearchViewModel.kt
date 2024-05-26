4a377cef2340edc5f557233ed3843167ae1e5bae
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-26 08:57:51 +0530

    refactor SearchViewModel added playTrack()

--- app/src/main/java/com/scarry/ammusicplayer/viewModels/searchViewModel/SearchViewModel.kt
+++ app/src/main/java/com/scarry/ammusicplayer/viewModels/searchViewModel/SearchViewModel.kt
@@ -3,28 +3,40 @@ package com.scarry.ammusicplayer.viewModels.searchViewModel
 import android.app.Application
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import coil.ImageLoader
+import coil.request.ImageRequest
+import coil.request.ImageResult
+import coil.request.SuccessResult
+import com.scarry.ammusicplayer.Domain.SearchResult
 import com.scarry.ammusicplayer.Domain.SearchResults
 import com.scarry.ammusicplayer.Domain.emptySearchResults
+import com.scarry.ammusicplayer.Domain.toMusicPlayerTrack
 import com.scarry.ammusicplayer.data.repository.Repository
 import com.scarry.ammusicplayer.data.utils.FetchedResource
 import com.scarry.ammusicplayer.data.utils.MapperImageSize
 import com.scarry.ammusicplayer.di.AM_MusicApplication
 import com.scarry.ammusicplayer.di.IoDispatcher
+import com.scarry.ammusicplayer.musicPlayer.MusicPlayer
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities.Private
 
 enum class SearchScreenUiState{LOADING, SUCCESS, IDLE}
 @HiltViewModel
 class SearchViewModel @Inject constructor(
     application: Application,
     private val repository: Repository,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    private val musicPlayer: MusicPlayer
 ) : AndroidViewModel(application) {
     private var searchJob: Job? = null
     private val emptySearchResults = emptySearchResults()
@@ -51,6 +63,11 @@ class SearchViewModel @Inject constructor(
         )
     }else _searchResults.value
 
+    private suspend fun downloadBitmapFromUrl(urlString: String) : ImageResult{
+        val imageRequest = ImageRequest.Builder(getApplication()).data(urlString).build()
+        return ImageLoader(getApplication()).execute(imageRequest)
+
+    }
     fun searchWithFilter(
         searchQuery: String,
         searchFilter: SearchFilter = SearchFilter.ALL
@@ -85,6 +102,21 @@ class SearchViewModel @Inject constructor(
     }
     fun applyFilterToSearchResult(searchFilter: SearchFilter) {
         filteredSearchResult.value = getSearchResultObjectForFilter(searchFilter)
+    }
+    fun playTrack(track : SearchResult.TrackSearchResult){
+        if(track.trackUrlString == null) return
+        viewModelScope.launch(ioDispatcher) {
+            _uiState.value = SearchScreenUiState.LOADING
+            val downloadAlbumArtResult = downloadBitmapFromUrl(track.imageUrlString)
+            if (downloadAlbumArtResult is SuccessResult){
+                withContext(Dispatchers.Main){
+                    musicPlayer.playTrack(track.toMusicPlayerTrack(downloadAlbumArtResult.drawable.toBitmap()))
+                }
+                _uiState.value = SearchScreenUiState.SUCCESS
+            }else{
+                _uiState.value =SearchScreenUiState.IDLE
+            }
+        }
     }
 
     fun getAvailableGenre()= repository.fetchAvailableGenre()
