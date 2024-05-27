86b92660d798d63ccd4143555b2cae72e371b409
aditay29mishra <adityarakeshmishra@gmail.com>
2024-05-27 15:26:35 +0530

    docs

--- app/src/main/java/com/scarry/ammusicplayer/useCase/playTrackUseCase/PlayTrackWithMediaNotificationUseCase.kt
+++ app/src/main/java/com/scarry/ammusicplayer/useCase/playTrackUseCase/PlayTrackWithMediaNotificationUseCase.kt
@@ -1,7 +1,11 @@
 package com.scarry.ammusicplayer.useCase.playTrackUseCase
 
 import com.scarry.ammusicplayer.Domain.SearchResult
-
+/**
+ * A use case that is responsible for playing an instance of
+ * [SearchResult.TrackSearchResult] and displaying
+ * a media notification.
+ */
 interface PlayTrackWithMediaNotificationUseCase {
     suspend fun invoke(
         track: SearchResult.TrackSearchResult,
